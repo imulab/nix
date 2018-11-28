35cca83ef83d54778e4eb484c9e1ff6653bd5b80
Weinan Qiu <davidiamyou@gmail.com>
2018-11-28 15:42:04 -0500

    OidcAuthorizeRequestRepository

---
+++ src/main/kotlin/io/imulab/nix/oidc/authn/Storage.kt
@@ -0,0 +1,47 @@
+package io.imulab.nix.oidc.authn
+
+import io.imulab.nix.oidc.OidcAuthorizeRequest
+
+/**
+ * Storage interface for saving an [OidcAuthorizeRequest] for re-entry.
+ */
+interface OidcAuthorizeRequestRepository {
+
+    /**
+     * Saves a [request]. Implementations are suggested to utilize a time-to-live strategy to evict
+     * the request after a certain time. This ensures the request will not be orphaned if request
+     * re-entry does not happen for some reason.
+     */
+    suspend fun save(request: OidcAuthorizeRequest)
+
+    /**
+     * Retrieves the request by its id. Implementation should return null if such request is not found.
+     */
+    suspend fun get(requestId: String): OidcAuthorizeRequest?
+
+    /**
+     * Deletes the request by its id. This usually happens after re-entry data has been processed and hence
+     * no longer needs the original request in repository.
+     */
+    suspend fun delete(requestId: String)
+}
+
+/**
+ * Memory implementation for [OidcAuthorizeRequestRepository]. This implementation is intended for
+ * development and/or testing purposes only. It does not follow the interface guideline to utilize
+ * a time-to-live configuration. It is not thread-safe either.
+ */
+class MemoryOidcAuthorizeRequestRepository : OidcAuthorizeRequestRepository {
+
+    private val db = mutableMapOf<String, OidcAuthorizeRequest>()
+
+    override suspend fun save(request: OidcAuthorizeRequest) {
+        db[request.id] = request
+    }
+
+    override suspend fun get(requestId: String): OidcAuthorizeRequest? = db[requestId]
+
+    override suspend fun delete(requestId: String) {
+        db.remove(requestId)
+    }
+}
