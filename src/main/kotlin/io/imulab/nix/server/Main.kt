0f05e36f6404eca79e578fa4325194d53b344cfc
Weinan Qiu <davidiamyou@gmail.com>
2018-12-01 17:22:37 -0500

    install ktor cookie feature

--- src/main/kotlin/io/imulab/nix/server/Main.kt
+++ src/main/kotlin/io/imulab/nix/server/Main.kt
@@ -1,12 +1,17 @@
 package io.imulab.nix.server
 
+import io.imulab.nix.server.authz.authn.session.AuthenticationSession
 import io.imulab.nix.server.route.authorize
 import io.imulab.nix.server.route.token
 import io.ktor.application.Application
+import io.ktor.application.install
 import io.ktor.config.ApplicationConfig
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.sessions.SessionStorageMemory
+import io.ktor.sessions.Sessions
+import io.ktor.sessions.cookie
 import io.ktor.util.KtorExperimentalAPI
 import org.kodein.di.Kodein
 import org.kodein.di.erased.bind
@@ -15,6 +20,16 @@ import org.kodein.di.erased.singleton
 
 @UseExperimental(KtorExperimentalAPI::class)
 fun Application.nix() {
+
+    install(Sessions) {
+        cookie<AuthenticationSession>(
+            name = "auth",
+            storage = SessionStorageMemory()
+        ) {
+            cookie.path = "/"
+            // TODO add encryption in the future so we can just store in user browser.
+        }
+    }
 
     val di = Kodein {
         bind<ApplicationConfig>() with singleton { environment.config }
