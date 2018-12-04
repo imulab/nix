acb94567e4ec897f80045697b1e863161507f05c
Weinan Qiu <davidiamyou@gmail.com>
2018-12-03 19:40:26 -0500

    OAuthImplicitHandler

---
+++ src/main/kotlin/io/imulab/nix/oauth/handler/OAuthImplicitHandler.kt
@@ -0,0 +1,36 @@
+package io.imulab.nix.oauth.handler
+
+import io.imulab.nix.oauth.OAuthContext
+import io.imulab.nix.oauth.exactly
+import io.imulab.nix.oauth.request.OAuthAuthorizeRequest
+import io.imulab.nix.oauth.reserved.GrantType
+import io.imulab.nix.oauth.reserved.ResponseType
+import io.imulab.nix.oauth.response.AuthorizeEndpointResponse
+import io.imulab.nix.oauth.token.storage.AccessTokenRepository
+import io.imulab.nix.oauth.token.strategy.AccessTokenStrategy
+
+class OAuthImplicitHandler(
+    private val oauthContext: OAuthContext,
+    private val accessTokenStrategy: AccessTokenStrategy,
+    private val accessTokenRepository: AccessTokenRepository
+) : AuthorizeRequestHandler {
+
+    override suspend fun handleAuthorizeRequest(request: OAuthAuthorizeRequest, response: AuthorizeEndpointResponse) {
+        if (!request.responseTypes.exactly(ResponseType.token))
+            return
+
+        request.client.mustGrantType(GrantType.implicit)
+
+        accessTokenStrategy.generateToken(request).also { accessToken ->
+            accessTokenRepository.createAccessTokenSession(accessToken, request)
+            response.accessToken = accessToken
+            response.tokenType = "bearer"
+            response.expiresIn = oauthContext.accessTokenLifespan.toSeconds()
+        }
+
+        response.scope = request.session.grantedScopes
+        response.state = request.state
+
+        response.handledResponseTypes.add(ResponseType.token)
+    }
+}
