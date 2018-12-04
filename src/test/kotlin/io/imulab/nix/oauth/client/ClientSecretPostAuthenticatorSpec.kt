ee5b5dc0535453802d060dc9d12266d0ec0c3db3
Weinan Qiu <davidiamyou@gmail.com>
2018-12-03 22:01:18 -0500

    refactor ClientSecretPostAuthenticatorSpec to gwt

--- src/test/kotlin/io/imulab/nix/oauth/client/ClientSecretPostAuthenticatorSpec.kt
+++ src/test/kotlin/io/imulab/nix/oauth/client/ClientSecretPostAuthenticatorSpec.kt
@@ -4,70 +4,76 @@ import com.nhaarman.mockitokotlin2.argThat
 import com.nhaarman.mockitokotlin2.doAnswer
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
+import io.imulab.nix.`when`
+import io.imulab.nix.given
+import io.imulab.nix.oauth.client.ClientSecretPostAuthenticatorSpec.requestWithCredential
+import io.imulab.nix.oauth.client.authn.ClientSecretPostAuthenticator
+import io.imulab.nix.oauth.client.pwd.BCryptPasswordEncoder
 import io.imulab.nix.oauth.error.InvalidClient
 import io.imulab.nix.oauth.error.OAuthException
 import io.imulab.nix.oauth.request.OAuthRequestForm
 import io.imulab.nix.oauth.reserved.Param
-import io.imulab.nix.oauth.client.authn.ClientSecretPostAuthenticator
-import io.imulab.nix.oauth.client.pwd.BCryptPasswordEncoder
+import io.imulab.nix.then
 import kotlinx.coroutines.runBlocking
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
-import java.nio.charset.StandardCharsets
 
 object ClientSecretPostAuthenticatorSpec: Spek({
 
-    val authenticator = ClientSecretPostAuthenticator(
-        lookup = BOM.clientLookup,
-        passwordEncoder = BOM.pwdEncoder
-    )
+    given("a properly configured authenticator") {
+        val authenticator = Given().authenticator
 
-    describe("authenticate client") {
-        it("should return client when credential is sound") {
-            val form = OAuthRequestForm(
-                httpForm = mutableMapOf(
-                    Param.clientId to listOf("foo"),
-                    Param.clientSecret to listOf("s3cret")
-                )
-            )
+        `when`("a request is made with valid credential") {
+            val request = requestWithCredential("foo", "s3cret")
 
-            runBlocking {
-                Assertions.assertThat(authenticator.authenticate(form)).extracting {
-                    it.id
-                }.isEqualTo("foo")
+            then("authentication should pass and return the client") {
+                runBlocking {
+                    assertThat(authenticator.authenticate(request)).extracting {
+                        it.id
+                    }.isEqualTo("foo")
+                }
             }
         }
 
-        it("should raise invalid_client error when credential is bad") {
-            val form = OAuthRequestForm(
-                httpForm = mutableMapOf(
-                    Param.clientId to listOf("foo"),
-                    Param.clientSecret to listOf("bad")
-                )
-            )
+        `when`("a request is made with bad credential") {
+            val request = requestWithCredential("foo", "invalid")
 
-            Assertions.assertThatExceptionOfType(OAuthException::class.java)
-                .isThrownBy {
-                    runBlocking { authenticator.authenticate(form) }
-                }
+            then("authentication should raise an error") {
+                assertThatExceptionOfType(OAuthException::class.java)
+                    .isThrownBy { runBlocking { authenticator.authenticate(request) } }
+            }
+        }
+
+        `when`("a request with made with unknown client") {
+            val request = requestWithCredential("bar", "s3cret")
+
+            then("authentication should raise an error") {
+                assertThatExceptionOfType(OAuthException::class.java)
+                    .isThrownBy { runBlocking { authenticator.authenticate(request) } }
+            }
         }
     }
 }) {
-    private object BOM {
-        val pwdEncoder = BCryptPasswordEncoder()
-
-        val fooClient = mock<OAuthClient> {
+    class Given {
+        private val encoder = BCryptPasswordEncoder()
+        private val client = mock<OAuthClient> {
             onGeneric { it.id } doReturn "foo"
-            onGeneric { it.secret } doReturn pwdEncoder.encode("s3cret").toByteArray(StandardCharsets.UTF_8)
+            onGeneric { it.secret } doReturn encoder.encode("s3cret").toByteArray()
         }
+        private val clientLookup = mock<ClientLookup> {
+            onBlocking { find("foo") } doReturn client
+            onBlocking { find(argThat { this != "foo" }) } doAnswer { throw InvalidClient.unknown() }
+        }
+        val authenticator = ClientSecretPostAuthenticator(clientLookup, encoder)
+    }
 
-        val clientLookup = mock<ClientLookup> {
-            onBlocking { find("foo") } doReturn fooClient
-            onBlocking { find(argThat { this != "foo" }) } doAnswer {
-                println(it.arguments[0])
-                throw InvalidClient.unknown()
-            }
-        }
+    fun requestWithCredential(username: String, password: String): OAuthRequestForm {
+        return OAuthRequestForm(
+            httpForm = mutableMapOf(
+                Param.clientId to listOf(username),
+                Param.clientSecret to listOf(password)
+            )
+        )
     }
 }
