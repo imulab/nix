841891717f77f8b23f3e94ceb2386754e7ebe734
Weinan Qiu <davidiamyou@gmail.com>
2018-12-03 22:15:37 -0500

    state validator to gwt

--- src/test/kotlin/io/imulab/nix/oauth/validation/StateValidatorSpec.kt
+++ src/test/kotlin/io/imulab/nix/oauth/validation/StateValidatorSpec.kt
@@ -2,45 +2,52 @@ package io.imulab.nix.oauth.validation
 
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
-import io.imulab.nix.oauth.*
+import io.imulab.nix.`when`
+import io.imulab.nix.given
 import io.imulab.nix.oauth.error.OAuthException
 import io.imulab.nix.oauth.request.OAuthAuthorizeRequest
 import io.imulab.nix.oauth.request.OAuthRequest
-import org.assertj.core.api.Assertions
+import io.imulab.nix.oauth.validation.StateValidatorSpec.requestWithState
+import io.imulab.nix.then
+import org.assertj.core.api.Assertions.assertThatCode
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
 
 object StateValidatorSpec : Spek({
 
-    fun withState(v: String): OAuthRequest {
+    given("a state validator with configured state entropy") {
+        val validator = StateValidator(
+            mock { onGeneric { stateEntropy } doReturn 8 }
+        )
+
+        `when`("a request is made with sufficient state entropy") {
+            val request = requestWithState("12345678")
+            then("validation should pass") {
+                assertThatCode { validator.validate(request) }
+                    .doesNotThrowAnyException()
+            }
+        }
+
+        `when`("a request is made with no state") {
+            val request = requestWithState("")
+            then("validation should pass, because state is optional") {
+                assertThatCode { validator.validate(request) }
+                    .doesNotThrowAnyException()
+            }
+        }
+
+        `when`("a request is made with insufficient state entropy") {
+            val request = requestWithState("123")
+            then("validation should fail") {
+                assertThatExceptionOfType(OAuthException::class.java)
+                    .isThrownBy { validator.validate(request) }
+            }
+        }
+    }
+}) {
+    fun requestWithState(v: String): OAuthRequest {
         return mock<OAuthAuthorizeRequest> {
             onGeneric { state } doReturn v
         }
     }
-
-    describe("validation") {
-
-        val context: OAuthContext = mock {
-            onGeneric { stateEntropy } doReturn 8
-        }
-
-        it("sufficient entropy should pass") {
-            Assertions.assertThatCode {
-                StateValidator(context).validate(withState("12345678"))
-            }.doesNotThrowAnyException()
-        }
-
-        it("no state should pass (because it's optional)") {
-            Assertions.assertThatCode {
-                StateValidator(context).validate(withState(""))
-            }.doesNotThrowAnyException()
-        }
-
-        it("insufficient entropy should fail") {
-            Assertions.assertThatExceptionOfType(OAuthException::class.java)
-                .isThrownBy {
-                    StateValidator(context).validate(withState("123"))
-                }
-        }
-    }
-})
+}
