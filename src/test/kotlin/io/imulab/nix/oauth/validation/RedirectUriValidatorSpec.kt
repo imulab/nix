4b4322a81af9f6cfed88d1d90414fc9726916d3c
Weinan Qiu <davidiamyou@gmail.com>
2018-12-03 22:08:18 -0500

    refactor RedirectUriValidatorSpec to gwt

--- src/test/kotlin/io/imulab/nix/oauth/validation/RedirectUriValidatorSpec.kt
+++ src/test/kotlin/io/imulab/nix/oauth/validation/RedirectUriValidatorSpec.kt
@@ -2,17 +2,74 @@ package io.imulab.nix.oauth.validation
 
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
-import io.imulab.nix.oauth.request.OAuthAuthorizeRequest
-import io.imulab.nix.oauth.error.OAuthException
-import io.imulab.nix.oauth.request.OAuthRequest
+import io.imulab.nix.`when`
+import io.imulab.nix.given
 import io.imulab.nix.oauth.client.OAuthClient
-import org.assertj.core.api.Assertions
+import io.imulab.nix.oauth.error.OAuthException
+import io.imulab.nix.oauth.request.OAuthAuthorizeRequest
+import io.imulab.nix.oauth.request.OAuthRequest
+import io.imulab.nix.oauth.validation.RedirectUriValidatorSpec.requestWithUri
+import io.imulab.nix.then
+import org.assertj.core.api.Assertions.assertThatCode
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
 
 object RedirectUriValidatorSpec: Spek({
 
-    fun withUri(v: String): OAuthRequest {
+    given("redirect uri validator") {
+        val validator = RedirectUriValidator
+
+        `when`("request made with good redirect uri") {
+            val request = requestWithUri("https://test.com/app")
+
+            then("validation should pass") {
+                assertThatCode { validator.validate(request) }.doesNotThrowAnyException()
+            }
+        }
+
+        `when`("request made with localhost uri") {
+            val request = requestWithUri("http://localhost:8080/app")
+
+            then("validation should pass") {
+                assertThatCode { validator.validate(request) }.doesNotThrowAnyException()
+            }
+        }
+
+        `when`("request made with 127.0.0.1 uri") {
+            val request = requestWithUri("http://127.0.0.1:8080/app")
+
+            then("validation should pass") {
+                assertThatCode { validator.validate(request) }.doesNotThrowAnyException()
+            }
+        }
+
+        `when`("request made with app uri") {
+            val request = requestWithUri("app://deep/link")
+
+            then("validation should pass") {
+                assertThatCode { validator.validate(request) }.doesNotThrowAnyException()
+            }
+        }
+
+        `when`("request made with http (non-localhost) uri") {
+            val request = requestWithUri("http://test.com")
+
+            then("validation should fail") {
+                assertThatExceptionOfType(OAuthException::class.java)
+                    .isThrownBy { validator.validate(request) }
+            }
+        }
+
+        `when`("request made with url with fragment") {
+            val request = requestWithUri("https://test.com?foo=bar#something")
+            then("validation should fail") {
+                assertThatExceptionOfType(OAuthException::class.java)
+                    .isThrownBy { validator.validate(request) }
+            }
+        }
+    }
+}) {
+    fun requestWithUri(v: String): OAuthRequest {
         val client: OAuthClient = mock {
             onGeneric { redirectUris } doReturn setOf(v)
         }
@@ -21,44 +78,4 @@ object RedirectUriValidatorSpec: Spek({
             onGeneric { this.client } doReturn client
         }
     }
-
-
-    describe("validation") {
-
-        it("https redirect uri should be valid") {
-            Assertions.assertThatCode {
-                RedirectUriValidator.validate(withUri("https://test.com/app"))
-            }.doesNotThrowAnyException()
-        }
-
-        it("http localhost or 127.0.0.1 uri should be valid") {
-            Assertions.assertThatCode {
-                RedirectUriValidator.validate(withUri("http://localhost:8080/app"))
-            }.doesNotThrowAnyException()
-
-            Assertions.assertThatCode {
-                RedirectUriValidator.validate(withUri("http://127.0.0.1:8080/app"))
-            }.doesNotThrowAnyException()
-        }
-
-        it("app redirect uri should be valid") {
-            Assertions.assertThatCode {
-                RedirectUriValidator.validate(withUri("app://deep/link"))
-            }.doesNotThrowAnyException()
-        }
-
-        it("non-localhost http redirect url is not valid") {
-            Assertions.assertThatExceptionOfType(OAuthException::class.java)
-                .isThrownBy {
-                    RedirectUriValidator.validate(withUri("http://test.com"))
-                }
-        }
-
-        it("uri with fragment is not valid") {
-            Assertions.assertThatExceptionOfType(OAuthException::class.java)
-                .isThrownBy {
-                    RedirectUriValidator.validate(withUri("https://test.com?foo=bar#something"))
-                }
-        }
-    }
-})
+}
