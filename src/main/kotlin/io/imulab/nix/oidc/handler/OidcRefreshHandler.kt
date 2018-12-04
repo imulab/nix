16439ccdad06c57538a0ff84c0f7d5972a60ca90
Weinan Qiu <davidiamyou@gmail.com>
2018-12-04 13:53:01 -0500

    OidcRefreshHandler

---
+++ src/main/kotlin/io/imulab/nix/oidc/handler/OidcRefreshHandler.kt
@@ -0,0 +1,46 @@
+package io.imulab.nix.oidc.handler
+
+import io.imulab.nix.oauth.handler.AccessRequestHandler
+import io.imulab.nix.oauth.request.OAuthAccessRequest
+import io.imulab.nix.oauth.reserved.GrantType
+import io.imulab.nix.oauth.response.TokenEndpointResponse
+import io.imulab.nix.oidc.client.OidcClient
+import io.imulab.nix.oidc.handler.helper.TokenHashHelper
+import io.imulab.nix.oidc.request.OidcSession
+import io.imulab.nix.oidc.reserved.IdTokenClaim
+import io.imulab.nix.oidc.reserved.StandardScope
+import io.imulab.nix.oidc.response.OidcTokenEndpointResponse
+import io.imulab.nix.oidc.token.IdTokenStrategy
+
+class OidcRefreshHandler(private val idTokenStrategy: IdTokenStrategy) : AccessRequestHandler {
+
+    override suspend fun updateSession(request: OAuthAccessRequest) {}
+
+    override suspend fun handleAccessRequest(request: OAuthAccessRequest, response: TokenEndpointResponse) {
+        if (!request.shouldBeHandled())
+            return
+
+        check(response is OidcTokenEndpointResponse) {
+            "Upstream should have supplied an OidcTokenEndpointResponse"
+        }
+
+        check(response.accessToken.isNotEmpty()) {
+            "Upstream should have generated an access token. Was handler misplaced?"
+        }
+
+        check(request.session is OidcSession) {
+            "Caller should have supplied an OidcSession."
+        }
+
+        check(request.client is OidcClient) {
+            "Caller should have supplied an OidcClient."
+        }
+
+        request.session.idTokenClaims[IdTokenClaim.accessTokenHash] =
+                TokenHashHelper.leftMostHash(response.accessToken, request.client.idTokenSignedResponseAlgorithm)
+        response.idToken = idTokenStrategy.generateToken(request)
+    }
+
+    private fun OAuthAccessRequest.shouldBeHandled(): Boolean =
+        grantTypes.contains(GrantType.refreshToken) && session.grantedScopes.contains(StandardScope.openid)
+}
